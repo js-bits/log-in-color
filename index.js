4d64515af2d73aeaca932a559b98ce8ed04b74e4
ApoloGH <apolo-gh@users.noreply.github.com>
2021-04-20 18:37:31 -0400

    refactor: fix linting

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ const COLOR_CODES = {
   blue: 34,
   magenta: 35,
   cyan: 36,
-  white: 37
+  white: 37,
 };
 
+const colorize = colorCode => message => `\x1b[${colorCode}m${message}\x1b[0m`;
+
+const methods = Object.keys(COLOR_CODES).reduce((acc, colorName) => {
+  acc[colorName] = colorize(COLOR_CODES[colorName]);
+  return acc;
+}, {});
+
+export const { black, red, green, yellow, blue, magenta, cyan, white } = methods;
+
 // https://habr.com/ru/post/492366/
-
-const methods = {};
-
-for (let colorName in COLOR_CODES) {
-  methods[colorName] = (colorCode => {
-    return message => `\x1b[${colorCode}m${message}\x1b[0m`;
-  })(COLOR_CODES[colorName]);
-}
-
-module.exports = methods;
