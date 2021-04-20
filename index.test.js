3c85ab152fef4f715d4031757d7339913fdcd87b
ApoloGH <apolo-gh@users.noreply.github.com>
2021-04-20 18:49:53 -0400

    test: add unit-tests

---
+++ index.test.js
@@ -0,0 +1,30 @@
+import { black, red, green, yellow, blue, magenta, cyan, white } from './index.js';
+
+const env = cyan(`[${typeof window === 'undefined' ? 'node' : 'jsdom'}]`);
+
+describe(`log-in-color ${env}`, () => {
+  test('black', () => {
+    expect(black('test')).toEqual('\x1b[30mtest\x1b[0m');
+  });
+  test('red', () => {
+    expect(red('test')).toEqual('\x1b[31mtest\x1b[0m');
+  });
+  test('green', () => {
+    expect(green('test')).toEqual('\x1b[32mtest\x1b[0m');
+  });
+  test('yellow', () => {
+    expect(yellow('test')).toEqual('\x1b[33mtest\x1b[0m');
+  });
+  test('blue', () => {
+    expect(blue('test')).toEqual('\x1b[34mtest\x1b[0m');
+  });
+  test('magenta', () => {
+    expect(magenta('test')).toEqual('\x1b[35mtest\x1b[0m');
+  });
+  test('cyan', () => {
+    expect(cyan('test')).toEqual('\x1b[36mtest\x1b[0m');
+  });
+  test('white', () => {
+    expect(white('test')).toEqual('\x1b[37mtest\x1b[0m');
+  });
+});
